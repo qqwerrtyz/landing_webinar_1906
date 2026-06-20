a3bd9e2cf4a6efea906944f2411cd4977394e67c
cruciffix <mr.unnona@gmail.com>
2026-06-20 13:35:40 +0300

    отображение формы

--- app/components/GetCourseWidget.jsx
+++ app/components/GetCourseWidget.jsx
@@ -2,8 +2,13 @@
 
 import { useEffect, useRef } from "react";
 
-const SCRIPT_ID = "05056b670cb2329a740b429c10c24237443072ca";
-const WIDGET_ID = "1618186";
+// const SCRIPT_ID = "05056b670cb2329a740b429c10c24237443072ca";
+// const WIDGET_ID = "1618186";
+
+const SCRIPT_ID = "7eccf9a80d8562842b77ed1b0e262a4c48456876";
+const WIDGET_ID = "1620521";
+
+
 const WIDGET_SRC = `https://novaya-planeta-academy.ru/pl/lite/widget/script?id=${WIDGET_ID}`;
 
 export default function GetCourseWidget() {
